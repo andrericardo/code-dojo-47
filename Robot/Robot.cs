15e65acfa1bfc94d4d9c0879a68ae9bba3c6ab5d
André Ricardo <andric87@gmail.com>
2020-01-29 20:01:38 +0000

    Constructor with input

--- Robot/Robot.cs
+++ Robot/Robot.cs
@@ -4,6 +4,13 @@ namespace Robot
 {
     public class Robot
     {
+        private string _input;
+
+        public Robot(string input)
+        {
+            _input = input;
+        }
+
         public int Add(int x, int y) {
             return x + y;
         }
--- RobotTest/RobotTest.cs
+++ RobotTest/RobotTest.cs
@@ -9,8 +9,17 @@ namespace RobotTest
         [Fact]
         public void Test1()
         {
-            var robot = new Class1();
+            var robot = new Robot.Robot();
             Assert.Equal(4, robot.Add(2, 2));
+        }
+
+        [Fact]
+        public void TestInput() 
+        {
+            const string input = @"10\nquit";
+
+            var robot = new Robot.Robot(input);
+
         }
     }
 }
